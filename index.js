b4aa61667009c670a229a2874fe14dcb68612ffb
tiwarigauri <gauri.uiet@gmail.com>
2024-09-03 00:34:18 +0530

    second commit

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ import { Router } from './routes/routes.js'
 const app = express()
 app.use(express.json())
 app.use(cors({
-    origin: ["https://contactms-client.vercel.app"],
+    origin: ["https://contactly-red.vercel.app"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true
 }))
